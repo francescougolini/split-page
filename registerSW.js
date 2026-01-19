32ee64065013169f22ddb64d94b74a59828a871e
Francesco Ugolini <20535446+francescougolini@users.noreply.github.com>
2026-01-19 17:34:34 +0100

    Update registerSW.js

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/split-page/sw.js', { scope: '/split-page/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/split-page/' })})}
