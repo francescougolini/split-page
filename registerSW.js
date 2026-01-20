38a744fce1dde89409ee75abec4dbfaf2178c5ea
francescougolini <20535446+francescougolini@users.noreply.github.com>
2026-01-20 10:19:23 +0100

    New share option

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+=======
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+>>>>>>> ffb4197 (New share option)
