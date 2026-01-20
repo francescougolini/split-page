4527455a1a97db66831f3c956ca103388b7f0622
francescougolini <20535446+francescougolini@users.noreply.github.com>
2026-01-20 11:10:35 +0100

    Web Share API implementation

--- registerSW.js
+++ registerSW.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
-=======
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
->>>>>>> ffb4197 (New share option)
